bf46e0a5e1c8b956b24d0076d98c6bd299ff9790
salvatoluice <salvatoluice5@gmail.com>
2024-11-21 17:55:01 +0300

    service routes

--- routes/serviceRoutes.js
+++ routes/serviceRoutes.js
@@ -2,14 +2,11 @@ const express = require('express');
 const router = express.Router();
 const serviceController = require('../controllers/serviceController');
 
-// Routes for services
-router.post('/', serviceController.createService); // Create a new service
-router.get('/', serviceController.getServices); // Get all services
-router.get('/:id', serviceController.getServiceById); // Get a single service by ID
-router.put('/:id', serviceController.updateService); // Update a service
-router.delete('/:id', serviceController.deleteService); // Delete a service
-
-// Route to get services by store ID
-router.get('/store/:storeId', serviceController.getServicesByStoreId); // Get services by store ID
+router.post('/', serviceController.createService);
+router.get('/', serviceController.getServices);
+router.get('/:id', serviceController.getServiceById); 
+router.put('/:id', serviceController.updateService);
+router.delete('/:id', serviceController.deleteService);
+router.get('/store/:storeId', serviceController.getServicesByStoreId);
 
 module.exports = router;
